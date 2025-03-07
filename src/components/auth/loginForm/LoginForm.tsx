86156e74ef071d025fd945522a96c422fdfedddf
harshitclub <harshitclub@gmail.com>
2025-03-07 14:25:03 +0530

    adding ui's

--- src/components/auth/loginForm/LoginForm.tsx
+++ src/components/auth/loginForm/LoginForm.tsx
@@ -8,11 +8,39 @@ import {
   SelectTrigger,
   SelectValue,
 } from "../../ui/select";
+import { useState } from "react";
+import { toast } from "sonner";
 function LoginForm() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    account: "",
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form Data:", user);
+    toast.success("Login Success");
+    setUser({
+      email: "",
+      password: "",
+      account: "",
+    });
+  };
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+  const handleSelectChange = (value: string) => {
+    setUser({ ...user, account: value });
+  };
   return (
     <>
       <div className="authForm width100 flex alignCenter justifyCenter flexColumn">
-        <form className="width60">
+        <form className="width60" onSubmit={handleSubmit}>
           <h1 className="scroll-m-20  pb-2 text-2xl font-bold tracking-tight first:mt-0 text-center">
             Login to your account
           </h1>
@@ -20,7 +48,14 @@ function LoginForm() {
             Enter your details below to login into your account
           </p>
           <h3 className="text-md font-semibold mb-1">Email</h3>
-          <Input type="email" placeholder="john@example.com" className="mb-3" />
+          <Input
+            type="email"
+            placeholder="john@example.com"
+            className="mb-3"
+            onChange={handleChange}
+            value={user.email}
+            name="email"
+          />
           <div className="authFormGroup flex spaceBtw mb-1">
             <h3 className="text-md font-semibold">Password</h3>
             <a
@@ -31,9 +66,16 @@ function LoginForm() {
             </a>
           </div>
 
-          <Input type="password" placeholder="**********" className="mb-3" />
+          <Input
+            type="password"
+            placeholder="**********"
+            className="mb-3"
+            onChange={handleChange}
+            value={user.password}
+            name="password"
+          />
           <h3 className="text-md font-semibold mb-1">Account?</h3>
-          <Select>
+          <Select value={user.account} onValueChange={handleSelectChange}>
             <SelectTrigger className="w-full mb-3">
               <SelectValue placeholder="Select Account" />
             </SelectTrigger>
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { Provider } from "react-redux";
+import store from "./redux/store/store.tsx";
 import { BrowserRouter } from "react-router";
 import { ThemeProvider } from "./components/shared/theme-provider.tsx";
+import { Toaster } from "./components/ui/sonner.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <Toaster position="top-center" richColors />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </Provider>
 );
